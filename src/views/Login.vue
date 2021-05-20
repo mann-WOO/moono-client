e3b09f59dec722b66039fc1ca1b50151488df171
mann_windows <6513032@gmail.com>
2021-05-20 22:00:45 +0900

    git add accounts app

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <p>ID</p>
+    <input type="text" v-model="credentials.username">
+    <p>PW</p>
+    <input type="password" @keyup.enter="getToken" v-model="credentials.password">
+    <br>
+    <br>
+    <button @click="getToken">Login</button>
+    <br>
+    <br>
+    <router-link :to="{ name:'Signup' }">Sign up</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      credentials: {
+        username: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    getToken: function () {
+      this.$store.dispatch('getToken', this.credentials)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Signup</h1>
+    <p>ID</p>
+    <input type="text" v-model="credentials.username">
+    <p>PW</p>
+    <input type="password" v-model="credentials.password">
+    <p>PW Confirmation</p>
+    <input type="password" @keyup.enter="signup" v-model="credentials.passwordConfirmation">
+    <br>
+    <br>
+    <button @click="signup">Signup</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+const SERVER_URL = process.env.VUE_APP_SERVER_URL
+
+export default {
+  name: 'Signup',
+  data: function () {
+    return {
+      credentials: {
+        username: '',
+        password: '',
+        passwordConfirmation: '',
+      }
+    }
+  },
+  methods: {
+    signup: function () {
+      axios({
+        method: 'post',
+        url: `${SERVER_URL}/accounts/signup/`,
+        data: this.credentials,
+      })
+        .then((res) => {
+          console.log(res)
+          this.$router.push({ name:'Login' })
+        })
+        .then((err) => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
